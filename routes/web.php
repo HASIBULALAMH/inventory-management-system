fbd5143a432c1c8c974a79d19e1a509898468064
hasibulalam <hasibulalam108@gmail.com>
2025-08-22 06:29:17 +0600

    admin crud operation

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\admin\DashboardController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::get('/', function () {
     return view('admin.master');
 });
+
+Route::get('dashboard',[DashboardController::class,'Dashboard'])->name('dashboard');
