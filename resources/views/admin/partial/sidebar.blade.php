55e9960f5ad844ff3ee74387574e60aa10bcc363
hasibulalam <hasibulalam108@gmail.com>
2025-08-28 21:07:53 +0600

    update admin template

--- resources/views/admin/partial/sidebar.blade.php
+++ resources/views/admin/partial/sidebar.blade.php
@@ -1,34 +1,105 @@
-<div class="sidebar" id="sidebar">
-    <div class="sidebar-header text-center py-4">
-        <img src="assets/img/logo.png" alt="Logo" style="width:60px;" class="mb-2 rounded-circle shadow-sm">
-        <h4 class="text-white">InventoryPro</h4>
+<nav id="sidebar" class="sidebar js-sidebar">
+    <div class="sidebar-content js-simplebar">
+        <a class="sidebar-brand" href="index.html">
+            <span class="align-middle">AdminKit</span>
+        </a>
+
+        <ul class="sidebar-nav">
+            <li class="sidebar-header">
+                Pages
+            </li>
+
+            <li class="sidebar-item active">
+                <a class="sidebar-link" href="index.html">
+                    <i class="align-middle" data-feather="sliders"></i> <span class="align-middle">Dashboard</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-profile.html">
+                    <i class="align-middle" data-feather="user"></i> <span class="align-middle">Profile</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-sign-in.html">
+                    <i class="align-middle" data-feather="log-in"></i> <span class="align-middle">Sign In</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-sign-up.html">
+                    <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Sign Up</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-blank.html">
+                    <i class="align-middle" data-feather="book"></i> <span class="align-middle">Blank</span>
+                </a>
+            </li>
+
+            <li class="sidebar-header">
+                Tools & Components
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="ui-buttons.html">
+                    <i class="align-middle" data-feather="square"></i> <span class="align-middle">Buttons</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="ui-forms.html">
+                    <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Forms</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="ui-cards.html">
+                    <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Cards</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="ui-typography.html">
+                    <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Typography</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="icons-feather.html">
+                    <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Icons</span>
+                </a>
+            </li>
+
+            <li class="sidebar-header">
+                Plugins & Addons
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="charts-chartjs.html">
+                    <i class="align-middle" data-feather="bar-chart-2"></i> <span class="align-middle">Charts</span>
+                </a>
+            </li>
+
+            <li class="sidebar-item">
+                <a class="sidebar-link" href="maps-google.html">
+                    <i class="align-middle" data-feather="map"></i> <span class="align-middle">Maps</span>
+                </a>
+            </li>
+        </ul>
+
+        <div class="sidebar-cta">
+            <div class="sidebar-cta-content">
+                <strong class="d-inline-block mb-2">Upgrade to Pro</strong>
+                <div class="mb-3 text-sm">
+                    Are you looking for more components? Check out our premium version.
+                </div>
+                <div class="d-grid">
+                    <a href="upgrade-to-pro.html" class="btn btn-primary">Upgrade to Pro</a>
+                </div>
+            </div>
+        </div>
     </div>
-    <ul class="nav flex-column">
-        <li class="nav-item">
-            <a class="nav-link active" href="{{ route('dashboard') }}"><i class="bi bi-speedometer2"></i> <span>Dashboard</span></a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link d-flex justify-content-between align-items-center" href="#" data-bs-toggle="collapse" data-bs-target="#productsMenu">
-                <span><i class="bi bi-box-seam"></i> Products</span>
-                <i class="bi bi-caret-down-fill"></i>
-            </a>
-            <ul class="collapse nav flex-column ms-3" id="productsMenu">
-                <li><a class="nav-link" href="#">All Products</a></li>
-                <li><a class="nav-link" href="#">Add Product</a></li>
-                <li><a class="nav-link" href="#">Inventory</a></li>
-            </ul>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link d-flex justify-content-between align-items-center" href="#" data-bs-toggle="collapse" data-bs-target="#categoriesMenu">
-                <span><i class="bi bi-tags-fill"></i> Categories</span>
-                <i class="bi bi-caret-down-fill"></i>
-            </a>
-            <ul class="collapse nav flex-column ms-3" id="categoriesMenu">
-                <li><a class="nav-link" href="#">All Categories</a></li>
-                <li><a class="nav-link" href="#">Add Category</a></li>
-            </ul>
-        </li>
-        <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-people-fill"></i> <span>Suppliers</span></a></li>
-        <li class="nav-item"><a class="nav-link" href="#"><i class="bi bi-bag-check-fill"></i> <span>Orders</span></a></li>
-    </ul>
-</div>
+</nav>
